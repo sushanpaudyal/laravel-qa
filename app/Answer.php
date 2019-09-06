c3d5ca2fd875ec51f78e820328815147492acce3
Sushan Paudyal <sushan.paudyal@gmail.com>
2019-09-06 23:09:21 +0545

    Displaying answer for question

--- app/Answer.php
+++ app/Answer.php
@@ -27,4 +27,8 @@ class Answer extends Model
         });
 
     }
+
+    public function getCreatedDateAttribute(){
+        return $this->created_at->diffForHumans();
+    }
 }
--- app/Question.php
+++ app/Question.php
@@ -43,4 +43,6 @@ class Question extends Model
         return $this->hasMany(Answer::class);
     }
 
+
+
 }
