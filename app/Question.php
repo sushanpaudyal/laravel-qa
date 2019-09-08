344bf957142907d54f94739333fa96afb3b8a0c9
Sushan Paudyal <sushan.paudyal@gmail.com>
2019-09-09 01:10:24 +0545

    Part 4

--- app/Question.php
+++ app/Question.php
@@ -52,15 +52,16 @@ class Question extends Model
         return $this->belongsToMany(User::class, 'favorites')->withTimestamps();
     }
 
-    public function isFavorited(){
-        return $this->favorites->where('user_id', auth()->id())->count() > 0;
+    public function isFavorited()
+    {
+        return $this->favorites()->where('user_id', auth()->id())->count() > 0;
     }
-
-    public function getIsFavoritedAttribute(){
+    public function getIsFavoritedAttribute()
+    {
         return $this->isFavorited();
     }
-
-    public function getFavoritesCountAttribute(){
+    public function getFavoritesCountAttribute()
+    {
         return $this->favorites->count();
     }
 
