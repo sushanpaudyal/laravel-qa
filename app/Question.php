421059fc5bcb331df86e57f73b29fca4e03b2f32
Sushan Paudyal <sushan.paudyal@gmail.com>
2019-09-09 00:07:54 +0545

    Accepting the answer as best answer part 1

--- app/Question.php
+++ app/Question.php
@@ -43,6 +43,11 @@ class Question extends Model
         return $this->hasMany(Answer::class);
     }
 
+    public function acceptBestAnswer(Answer $answer){
+        $this->best_answer_id = $answer->id;
+        $this->save();
+    }
+
 
 
 }
