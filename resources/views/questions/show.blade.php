626629943717d713a07588756afb0941a99418ce
Sushan Paudyal <sushan.paudyal@gmail.com>
2019-09-09 12:00:57 +0545

    Refactorring the view part 1

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -59,17 +59,17 @@
                             </div>
                             <div class="media-body">
                                 {!! $question->body_html !!}
-                                <div class="float-right">
-                                    <span class="text-muted">Answered {{$question->created_date}}</span>
-                                    <div class="media mt-2">
-                                        <a href="{{$question->user->url}}" class="pr-2">
-                                            <img src="{{$question->user->avatar}}" alt="">
-                                        </a>
-                                        <div class="media-body mt-1">
-                                            <a href="{{$question->user->url}}">{{$question->user->name}}</a>
-                                        </div>
-                                    </div>
-                                </div>
+                                  <div class="row">
+                                      <div class="col-4">
+                                      </div>
+                                      <div class="col-4"></div>
+                                      <div class="col-4">
+                                          @include('shared._author', [
+                                            'model' => $question,
+                                            'label' => 'asked'
+                                          ])
+                                      </div>
+                                  </div>
                             </div>
                         </div>
                     </div>
