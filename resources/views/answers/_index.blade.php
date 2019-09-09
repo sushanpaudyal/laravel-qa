1ac7eb5639b2af9f5155a8878fbcdbce845dabbe
Sushan Paudyal <sushan.paudyal@gmail.com>
2019-09-09 16:09:15 +0545

    Tidying up our views

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -10,66 +10,7 @@
                     @include('layouts._messages')
 
                     @foreach($answers as $answer)
-                        <div class="media">
-                            <div class="d-flex flex-column vote-controls">
-
-                                <a title="This answer is useful" class="vote-up {{ Auth::guest() ? 'off' : ''  }}"
-                                   onclick="event.preventDefault(); document.getElementById('up-vote-answer-{{ $answer->id }}').submit();">
-                                    <i class="fas fa-caret-up fa-3x"></i>
-                                </a>
-                                <span class="votes-count">{{$answer->votes_count}}</span>
-                                <form id="up-vote-answer-{{ $answer->id }}" action="/answers/{{ $answer->id }}/vote" method="POST" style="display:none;">
-                                    @csrf
-                                    <input type="hidden" name="vote" value="1">
-                                </form>
-
-
-                                <a title="This answer is not usable" class="vote-down {{ Auth::guest() ? 'off' : ''  }}"
-                                   onclick="event.preventDefault(); document.getElementById('down-vote-answer-{{ $answer->id }}').submit();">
-                                    <i class="fas fa-caret-down fa-3x"></i>
-                                </a>
-                                <form id="down-vote-answer-{{ $answer->id }}" action="/answers/{{ $answer ->id }}/vote" method="POST" style="display:none;">
-                                    @csrf
-                                    <input type="hidden" name="vote" value="-1">
-                                </form>
-
-
-
-                            </div>
-                            <div class="media-body">
-                                {{  $answer->body }}
-
-                                <div class="row">
-                                    <div class="col-4">
-                                        <div class="ml-auto">
-                                            @can('update', $answer)
-                                                <a href="{{route('questions.answers.edit',[$question->id, $answer->id])}}"class="btn btn-sm btn-outline-info">
-                                                    Edit
-                                                </a>
-                                            @endcan
-                                            @can('delete', $answer)
-                                                <form class="form-delete" action="{{route('questions.answers.destroy',[$question->id, $answer->id] )}}" method="post">
-                                                    {{method_field('DELETE')}}
-                                                    @csrf
-                                                    <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are You Sure ?')">
-                                                        Delete
-                                                    </button>
-                                                </form>
-                                            @endcan
-                                        </div>
-                                    </div>
-                                    <div class="col-4"></div>
-                                    <div class="col-4">
-                                        @include('shared._author', [
-                                           'model' => $answer,
-                                           'label' => 'answered'
-                                        ])
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-                        <hr>
+                       @include('answers._answer')
                     @endforeach
                 </div>
             </div>
